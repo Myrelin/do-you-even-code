37a158ac68abd8facd2aa5810d447ae185567d0c
myrelin <anna.iklody@gmail.com>
2019-07-01 20:50:59 +0200

    import refactor WIP

--- do_you_even_code_tool.py
+++ do_you_even_code_tool.py
@@ -1,29 +1,27 @@
 import importlib
 import logging
+import sys
 import time
 import os
+from modules import *
+
 
 logger = logging.getLogger('doyouevencode')
 process_names = ["pycharm.sh", "idea.sh"]
 
-try:
-    from .modules import *
-    HAS_PACKAGE_MODULES = True
-except Exception as e:
-    print(e)
-    HAS_PACKAGE_MODULES = False
+# try:
+#     from modules import *
+#     HAS_PACKAGE_MODULES = True
+# except Exception as e:
+#     print(e)
+#     HAS_PACKAGE_MODULES = False
 
 def main():
-    modules = load_modules()
-    import sys
-
-    modulename = 'process_checker'
-    if modulename not in sys.modules:
-        print('You have not imported the {} module'.format(modulename))
-
+    all_modules = load_modules()
     while True:
-        for module in modules:
-            modules[module].check()
+        for module in all_modules:
+
+            all_modules[module].check()
             time.sleep(5)
 
 def load_modules():
@@ -32,16 +30,38 @@ def load_modules():
     for file_name in filenames:
         if "__" not in file_name:
             module_name = file_name.split(".")[0]
-            module_instance = convert_filename_to_classname(module_name)
-            print(module_name, module_instance)
-            # imported_modules.update({module_name: convert_filename_to_classname(file_name)})
+            module_instance = convert_filename_to_classname(file_name)
             try:
-
-                imported_modules[module_name] = __import__('modules.' + module_name)
+                imported_modules[module_name] = importlib.import_module('modules.' + module_name)
             except ModuleNotFoundError:
                 print("module not found for some fucking reason")
     print(imported_modules)
+    sys.modules.update(imported_modules)
     return imported_modules
+
+
+
+#
+# def load_modules(mod_dir):
+#     sys.path.append(mod_dir)
+#     my_modules = {}
+#     for root, dirnames, filenames in os.walk(mod_dir):
+#         if os.path.basename(root) == '__pycache':
+#             continue
+#         if os.path.basename(root).startswith("."):
+#             continue
+#         for filename in fnmatch.filter(filenames, '*.py'):
+#             if root.split('/')[-1].startswith('_'):
+#                 continue
+#             if filename == '__init.py__':
+#                 continue
+#             module_name = filename.split(".")[0]
+#             try:
+#                 my_modules[module_name] = importlib.import_module(os.path.basename(root) + '.' + module_name)
+#             except Exception as e:
+#                 logger.warning('Tool modules {} failed due to {}'.format(module_name, e))
+#     return my_modules
+
 
 
 def convert_filename_to_classname(file_name):
